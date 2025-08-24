56f63d32d21c9dc6e4ffca6f66e0fa8daf24d0b2
Aryan <aryan182007@gmail.com>
2025-08-24 23:05:38 +0530

    2nd java Program

---
+++ 2nd-java/README.md
@@ -0,0 +1,1 @@
+# Using SCANER CLASS to take Input-----
---
+++ 2nd-java/first.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class first {
+    public static void main(String[] args){
+        System.out.println("KRPTONOX IS BACK AGAIN");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input);
+
+        int number = sc.nextInt();
+        System.out.println(number);
+        Boolean tr = sc.nextBoolean();
+        System.out.println(tr);
+    }
+}
