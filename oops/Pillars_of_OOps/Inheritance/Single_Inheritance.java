25b2e0c85325c79e2398dbded25b658c1fefef79
krptonox <aryanyadav182007@gmail.com>
2026-05-25 13:32:25 +0530

    New updates

--- oops/Pillars_of_OOps/Inheritance/Single_Inheritance.java
+++ oops/Pillars_of_OOps/Inheritance/Single_Inheritance.java
@@ -1,19 +1,21 @@
+package Inheritance;
+
 class Animal {
     void eat() {
-        System.out.println("Animal eats food");
+        System.out.println("Inheritance.Animal eats food");
     }
 }
 
 class Dog extends Animal {
     void bark() {
-        System.out.println("Dog barks");
+        System.out.println("Inheritance.Dog barks");
     }
 }
 
 public class Single_Inheritance {
     public static void main(String[] args) {
         Dog d = new Dog();
-        d.eat();   // inherited from Animal
+        d.eat();   // inherited from Inheritance.Animal
         d.bark();  // own method
     }
 }
